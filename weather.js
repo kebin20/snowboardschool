5d0a17e46e0ed5e41af4ff3cc762f7314bfc458a
kebin20 <ktanzyl@hotmail.com>
2022-08-29 15:19:28 +0900

    Edited the CSS a bit, added API in JS

---
+++ weather.js
@@ -0,0 +1,19 @@
+let weather = {
+    apikey: "1072c58fb827ff0250d45ffdfde36dcc",
+    fetchWeather: function (city) {
+        fetch(
+            `https://api.openweathermap.org/data/3.0/onecall?q=${city}&@units={metric}&appid=${this.apikey}`
+        )
+            .then((response) => response.json())
+            .then(({ data }) => console.log(data));
+
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { snow } = data.snow;
+
+
+    }
+}
